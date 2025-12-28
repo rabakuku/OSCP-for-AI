0a4a8ddf7854ed1d166178e350aa0901d719d43f
rabakuku <71357873+rabakuku@users.noreply.github.com>
2025-12-28 12:18:43 -0500

    Update 16_🛡️_DoS_Fixed.py

--- Section 0: Lab Code 🧪/pages/16_🛡️_DoS_Fixed.py
+++ Section 0: Lab Code 🧪/pages/16_🛡️_DoS_Fixed.py
@@ -6,6 +6,22 @@ st.success("🛡️ STATUS: SECURE (Token Limits)")
 
 user_input = st.text_input("Enter prompt:", "Repeat the word 'Company' forever.")
 
+# Show the secure code
+st.subheader("THE VULNERABILITY: Model Denial of Service")
+st.code("""
+    # FIX: Enforce max_tokens
+    # Even if requested to go forever, the API cuts it off.
+    limit = 50
+    st.info(f"Enforcing Hard Limit: {limit} tokens")
+
+    response = client.chat.completions.create(
+        model=MODEL_NAME,
+        messages=[{"role": "user", "content": user_input}],
+        max_tokens=limit
+        
+""", language="python")
+
+
 if st.button("Send Securely"):
     # FIX: Enforce max_tokens
     # Even if requested to go forever, the API cuts it off.
