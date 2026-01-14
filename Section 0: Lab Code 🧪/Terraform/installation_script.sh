f3a9778dace1767945fe4aad5180252315327d55
rabakuku <71357873+rabakuku@users.noreply.github.com>
2026-01-13 21:39:47 -0500

    Create installation_script.sh

---
+++ Section 0: Lab Code 🧪/Terraform/installation_script.sh
@@ -0,0 +1,8 @@
+   #! /bin/bash
+    echo "✅ Installation of Streamlit
+    # Make executable and run
+    chmod +x setup.sh
+    ./setup.sh
+    echo "✅ Installation of Streamlit is Complete!"
+
+    echo "✅ Installation of garak + miniconda
